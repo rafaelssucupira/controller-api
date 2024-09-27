ae66b5fb4a61eb23b86f4a193088981f9f2d62f5
rafaelssucupira <rafaelssucupira@gmail.com>
2024-09-27 13:58:27 -0300

    update

--- index.php
+++ index.php
@@ -9,13 +9,14 @@ define( "PARAMETERS", json_decode(file_get_contents("php://input"), true) );
 require_once(
     "vendor/autoload.php"
 );
-
-// use EController\Assets\Authentication;
-// use EController\Preview;
-// use EController\Transmit;
-// use EController\Consult;
-// use EController\Cancel;
-// use EController\ConsultDfe;
+use ControllerApi\Assets\Buy;
+use ControllerApi\Assets\Sale;
+use ControllerApi\Assets\Clients;
+use ControllerApi\Assets\Products;
+use ControllerApi\Assets\Authentication;
+use ControllerApi\Assets\Tax;
+use ControllerApi\Assets\Users;
+use ControllerApi\Assets\Password;
 
 //.ENV
 $env = Dotenv::createImmutable( __DIR__ . "/src/Enviroments/" );
@@ -26,39 +27,21 @@ $decoded = Authentication::decoded( $_SERVER["HTTP_AUTHORIZATION"] );
 if( $decoded["statments"] === "OK" )
 {
 
-    // $decoded = array( //mock
-    //     "dataEmp" => array(
-    //         "emp_codigo" => "CFR",
-    //         "emp_banco" => "rastza11_controller_cfr",
-    //     )    
-    // );
-    
-
     define("DB", $decoded["dataEmp"] );
     if ( isset(constant("PARAMETERS")["action"]) && !empty(constant("PARAMETERS")["action"]) )
         {
-            
-            $class = array(
-                "Products" => function () {
-                    return new Products();
-                },
-                "Transmit" => function () {
-                    return new Transmit();
-                },
-                "Consult" => function () {
-                    return new Consult();
-                },
-                "Cancel" => function () {
-                    return new Cancel();
-                },
-                "ConsultDfe" => function () {
-                    return new ConsultDfe();
-                }     
-            );
-
-            $action = constant("PARAMETERS")["action"];
-            $result = $class[$action]();
+            try {
+                $action = constant("PARAMETERS")["action"];
+                $class = new $action();
+            }
+            catch(\Exception $e ) {
+                echo json_encode(array(
+                    "statments" => "warning",
+                    "message" => $e->getMessage()
+                ));
+            }
             exit;
+            
         }
 }
 else
