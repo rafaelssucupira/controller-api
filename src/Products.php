533df2daa4fd997d744c1ad11fa73afb7d6a3c2e
rafaelssucupira <rafaelssucupira@gmail.com>
2024-09-28 08:46:18 -0300

    add FluentSQL e Querys

--- src/Products.php
+++ src/Products.php
@@ -1,11 +1,26 @@
 <?php
 
 namespace ControllerApi\Server;
+use FluentSQL\SQL;
+use Querys\Register\SqlsPro;
 
 class Products {
 
     function __construct() {
-        echo "Products class has been initialized";
+        $this->$data    = constant("PARAMETERS");
+        $router         = constant("PARAMETERS")["router"];
+        $this->$router();
+    }
+
+    public function select() 
+    {
+        $conn       = new SQL( $_ENV['DB_ENDPOINT'], $_ENV['DB_ADMIN'],  $_ENV['DB_USER'], $_ENV['DB_PASSWORD'] );
+        $result     = $conn
+                        ->prepareQuery(SqlsPro::SELECT(), [])
+                        ->execQuery()
+                        ->build(true);
+
+        echo json_encode($result);
 
     }
 
